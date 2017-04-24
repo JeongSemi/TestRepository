726461624c3769f240343ca0fc32db16c2b7c88a
JeongSemi <wjdtpa2@gmail.com>
2017-04-24 12:34:03 +0900

    ClassA 수정함

--- ClassA.java
+++ ClassA.java
@@ -1,8 +1,5 @@
 public class ClassA{
   public static void main(String[] args){
-    System.out.println("User1이 작성");
-    System.out.println("User2가 작성");
-    System.out.println("User2가 작성2");
-    System.out.println("User2가 작성3");
+    
   }
 }
