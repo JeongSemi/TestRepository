b899144580231158e579c4569fcf56d6aee5ae5a
JeongSemi <wjdtpa2@gmail.com>
2017-04-26 16:06:32 +0900

    Bootstrap

--- WebApplication/src/java/com/mycompany/myapp/HomeController.java
+++ WebApplication/src/java/com/mycompany/myapp/HomeController.java
@@ -51,4 +51,19 @@ public class HomeController {
     public String javascriptExam04() {
         return "javascript/exam04"; //리턴값.jsp가 실행된다
     }
+    
+    @RequestMapping("/bootstrap/exam01")
+    public String bootstrapExam01() {
+        return "bootstrap/exam01"; //리턴값.jsp가 실행된다
+    }
+    
+    @RequestMapping("/bootstrap/exam02")
+    public String bootstrapExam02() {
+        return "bootstrap/exam02"; //리턴값.jsp가 실행된다
+    }
+    
+    @RequestMapping("/bootstrap/exam03")
+    public String bootstrapExam03() {
+        return "bootstrap/exam03"; //리턴값.jsp가 실행된다
+    }   
 }
