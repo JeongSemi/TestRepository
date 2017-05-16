d3197486e244082b54f1685c19513eb2f08f6158
JeongSemi <wjdtpa2@gmail.com>
2017-05-16 17:49:47 +0900

    5/16

---
+++ Hardware/src/hardware/led/RgbLed.java
@@ -0,0 +1,56 @@
+package hardware.led;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class RgbLed {
+    
+    private GpioPinDigitalOutput redPin;
+    private GpioPinDigitalOutput greenPin;
+    private GpioPinDigitalOutput bluePin;
+
+    public RgbLed(Pin redPinNo, Pin greenPinNo, Pin bluePinNo) {
+        GpioController gpioController = GpioFactory.getInstance();
+        redPin = gpioController.provisionDigitalOutputPin(redPinNo, PinState.HIGH);
+        redPin.setShutdownOptions(true, PinState.LOW);
+        greenPin = gpioController.provisionDigitalOutputPin(greenPinNo, PinState.HIGH);
+        greenPin.setShutdownOptions(true, PinState.LOW);
+        bluePin = gpioController.provisionDigitalOutputPin(bluePinNo, PinState.HIGH);
+        bluePin.setShutdownOptions(true, PinState.LOW);
+        
+    }
+    
+    public void red(){
+        redPin.low();
+        greenPin.high();
+        bluePin.high();
+    }
+    
+    public void green(){
+        redPin.high();
+        greenPin.low();
+        bluePin.high();
+    }
+    
+    public void blue(){
+        redPin.high();
+        greenPin.high();
+        bluePin.low();
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        RgbLed rgbLed = new RgbLed(RaspiPin.GPIO_27, RaspiPin.GPIO_28, RaspiPin.GPIO_29);
+        while(true){
+            rgbLed.red();
+            Thread.sleep(1000);
+            rgbLed.green();
+            Thread.sleep(1000);
+            rgbLed.blue();
+            Thread.sleep(1000);
+        }
+    }
+}
---
+++ WebApplication/src/java/com/mycompany/myapp/dao/Exam10Dao.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Exam10Dao {
+
+    public void insert();
+
+    public void select();
+
+}
