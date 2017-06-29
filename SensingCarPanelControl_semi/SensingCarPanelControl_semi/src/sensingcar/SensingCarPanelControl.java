66e406618052e9133e412d0ae86f0d4edf1f62f2
JeongSemi <wjdtpa2@gmail.com>
2017-06-29 18:06:49 +0900

    6/29

--- SensingCarPanelControl_semi/SensingCarPanelControl_semi/src/sensingcar/SensingCarPanelControl.java
+++ SensingCarPanelControl_semi/SensingCarPanelControl_semi/src/sensingcar/SensingCarPanelControl.java
@@ -10,7 +10,7 @@ public class SensingCarPanelControl extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("motor/Video.fxml")); //AppMain.class.getResource() 똑같음
+        Parent parent = FXMLLoader.load(getClass().getResource("motor/Motor.fxml")); //AppMain.class.getResource() 똑같음
         Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
         primaryStage.setTitle("SensingCarPanelControl");
