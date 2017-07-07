8a00147f7fa4d100c151bd4933ba38997bcabeb7
JeongSemi <wjdtpa2@gmail.com>
2017-07-07 17:48:30 +0900

    7/7

--- IoTWebProgramming/src/main/java/com/mycompany/myapp/controller/HttpController.java
+++ IoTWebProgramming/src/main/java/com/mycompany/myapp/controller/HttpController.java
@@ -1,8 +1,14 @@
 package com.mycompany.myapp.controller;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/http")
 public class HttpController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HttpController.class);
 
 	@RequestMapping("/exam01")
 	public String exam01(@RequestParam(defaultValue = "0") double thermistor,
@@ -39,4 +47,56 @@ public class HttpController {
 		return "http/exam02";
 	}
 
+	@RequestMapping("/exam03")
+	public void exam03(String param1, String param2, HttpServletResponse response) throws Exception {
+		logger.info(param1);
+		logger.info(param2);
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("param1", param1);
+		jsonObject.put("param2", param2);
+		String json = jsonObject.toString();
+
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
+	}
+
+	@RequestMapping("/exam04")
+	public void exam04(String param1, String param2, MultipartFile attach, HttpServletResponse response)
+			throws Exception {
+		logger.info(param1);
+		logger.info(param2);
+		logger.info(attach.getOriginalFilename());
+		logger.info(attach.getContentType());
+		logger.info(String.valueOf(attach.getSize()));
+
+		String savedfilename = new Date().getTime() + "-" + attach.getOriginalFilename();
+		String savedfilepath = "C:/Temp/" + savedfilename;
+		attach.transferTo(new File(savedfilepath));
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("param1", param1);
+		jsonObject.put("param2", param2);
+		jsonObject.put("fileName", attach.getOriginalFilename());
+		jsonObject.put("fileType", attach.getContentType());
+		jsonObject.put("fileSize", attach.getSize());
+		String json = jsonObject.toString();
+
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
+	}
+
 }
+
+
+
+
+
+
+
